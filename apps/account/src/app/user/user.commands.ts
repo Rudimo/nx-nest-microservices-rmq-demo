05d77eec205fd7233ff018c9d0e4a25515e6734b
Dmitry Kshevsky <rudimo@yandex.ru>
2023-05-12 23:56:41 +0600

    small update

--- apps/account/src/app/user/user.commands.ts
+++ apps/account/src/app/user/user.commands.ts
@@ -1,35 +1,40 @@
 import { Body, Controller } from '@nestjs/common';
-import { AccountBuySubscription, AccountChangeProfile, AccountCheckPayment } from '@nx-monorepo-project/contracts';
+import {
+  AccountBuySubscription,
+  AccountChangeProfile,
+  AccountCheckPayment,
+} from '@nx-monorepo-project/contracts';
 import { RMQValidate, RMQRoute } from 'nestjs-rmq';
 import { UserService } from './user.service';
 
 @Controller()
 export class UserCommands {
+  constructor(private readonly userService: UserService) {}
 
-    constructor(private readonly userService: UserService) {
-    }
+  @RMQValidate()
+  @RMQRoute(AccountChangeProfile.topic)
+  async userProfile({
+    user,
+    id,
+  }: AccountChangeProfile.Request): Promise<AccountChangeProfile.Response> {
+    return this.userService.changeProfile(user, id);
+  }
 
-    @RMQValidate()
-    @RMQRoute(AccountChangeProfile.topic)
-    async userProfile(
-      @Body() { user, id }: AccountChangeProfile.Request
-    ): Promise<AccountChangeProfile.Response> {
-      return this.userService.changeProfile(user, id);
-    }
+  @RMQValidate()
+  @RMQRoute(AccountBuySubscription.topic)
+  async buySubscription({
+    userId,
+    subscriptionId,
+  }: AccountBuySubscription.Request): Promise<AccountBuySubscription.Response> {
+    return this.userService.buySubscription(userId, subscriptionId);
+  }
 
-    @RMQValidate()
-    @RMQRoute(AccountBuySubscription.topic)
-    async buySubscription(
-      @Body() { userId, subscriptionId }: AccountBuySubscription.Request
-    ): Promise<AccountBuySubscription.Response> {
-      return this.userService.buySubscription(userId, subscriptionId);
-    }
-
-    @RMQValidate()
-    @RMQRoute(AccountCheckPayment.topic)
-    async checkPayment(
-      @Body() { userId, subscriptionId }: AccountCheckPayment.Request
-    ): Promise<AccountCheckPayment.Response> {
-      return this.userService.checkPayments(userId, subscriptionId);
-    }
+  @RMQValidate()
+  @RMQRoute(AccountCheckPayment.topic)
+  async checkPayment({
+    userId,
+    subscriptionId,
+  }: AccountCheckPayment.Request): Promise<AccountCheckPayment.Response> {
+    return this.userService.checkPayments(userId, subscriptionId);
+  }
 }
